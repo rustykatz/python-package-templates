c2b4d592aaef1c85a73c6dc686b3a455421847ec
Russell Wong <rww6@sfu.ca>
2024-07-21 22:39:51 -0700

    Add simple tests

---
+++ tests/test_pyproject_hatchling_template.py
@@ -0,0 +1,12 @@
+
+
+from pyproject_hatchling_package.module import module_fxn
+from pyproject_hatchling_package.nested_package.nested_module import nested_module_fxn
+
+def test_import_module():
+    res = module_fxn()
+    assert str(res), "Failed to import module"
+
+def test_import_nested_module():
+    res = nested_module_fxn()
+    assert str(res), "Failed to import nested module"
